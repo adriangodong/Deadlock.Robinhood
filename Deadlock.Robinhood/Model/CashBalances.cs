b6c334f685acc76499b8848dd82ff5a5ea2b0674
Adrian Godong <adrian.godong@gmail.com>
2017-05-01 18:39:02 -0700

    Added cash_balances fields

--- Deadlock.Robinhood/Model/CashBalances.cs
+++ Deadlock.Robinhood/Model/CashBalances.cs
@@ -9,5 +9,30 @@ namespace Deadlock.Robinhood.Model
 {
     public class CashBalances
     {
+
+        [JsonProperty("cash_held_for_orders")]
+        public decimal CashHeldForOrders { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("cash")]
+        public decimal Cash { get; set; }
+
+        [JsonProperty("buying_power")]
+        public decimal BuyingPower { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("cash_available_for_withdrawal")]
+        public decimal CashAvailableForWithdrawal { get; set; }
+
+        [JsonProperty("uncleared_deposits")]
+        public decimal UnclearedDeposits { get; set; }
+
+        [JsonProperty("unsettled_funds")]
+        public decimal UnsettledFunds { get; set; }
+
     }
 }
